4d4af777149fa869785ebbdb782fdc0a9fd00572
Artur Nowak <arturmareknowal@gmail.com>
2020-08-11 22:15:34 +0200

    Added NSIS

---
+++ Build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/artur/OneDrive/Desktop/HelloWorld
+# Build directory: C:/Users/artur/OneDrive/Desktop/HelloWorld/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test1 "C:/Users/artur/OneDrive/Desktop/HelloWorld/Build/HelloWorld")
+set_tests_properties(test1 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/artur/OneDrive/Desktop/HelloWorld/CMakeLists.txt;12;add_test;C:/Users/artur/OneDrive/Desktop/HelloWorld/CMakeLists.txt;0;")
